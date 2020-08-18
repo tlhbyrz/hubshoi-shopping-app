82d1d2caae3bb540caf862434fccad34ded90259
tlhbyrz <talhaboyraz13@gmail.com>
2020-08-19 02:42:19 +0300

    footer tasarımına başlandı

---
+++ src/components/TrendProduct/TrendProduct.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./TrendProduct.css"
+
+
+function TrendProduct() {
+    return (
+        <div className="trend-product-card">
+            <img src={"https://www.biobizz.com/wp-content/uploads/2019/07/Bio%C2%B7pH-Product-Page-900x604.jpg"}
+                 className="trend-product-img"   />
+            <h5>Save up to 1/2 price</h5>
+            <p className="trend-add-to-card-button">Sepete Ekle</p>
+        </div>
+    )
+}
+
+export default TrendProduct
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React, {useState} from "react";
 import InfoCard from "../../components/InfoCard/InfoCard";
 import ProductCard from "../../components/ProductCard/ProductCard";
+import TrendProduct from "../../components/TrendProduct/TrendProduct";
 import "./Home.css"
 
 function Home() {
     const [cards, setCards] = useState([1,1,1,1,1,1]);
+    const [trendCards, setTrendCards] = useState([1,1,1,1]);
     const [loading, setLoading] = useState(true);
 
     return (
@@ -14,6 +16,19 @@ function Home() {
                     <ProductCard key={index}/>
                 ))
             }
+
+            <section className="sub-title">
+                <p className="sub-title-text">Amazing value on our trending products</p>
+            </section>
+
+            <section className="trend-product-container">
+                {
+                    trendCards.map((card, index) => (
+                        <TrendProduct key={index}/>
+                    ))
+                }
+            </section>
+
         </div>
     )
 }
