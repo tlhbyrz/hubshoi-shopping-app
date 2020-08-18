8531cd8a1f23d1a3ec450a9e84a5f7be176be816
tlhbyrz <talhaboyraz13@gmail.com>
2020-08-18 17:37:34 +0300

    card tasarımına başlandı

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Footer from "./components/Footer/Footer";
 export default function App() {
     return (
         <Router>
-            <div>
+            <div className="App">
                 <Navbar/>
 
                 <div className="app-container">
--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -4,7 +4,13 @@ import "./ProductCard.css"
 
 function ProductCard() {
     return (
-        <div>ProductCard</div>
+        <div className="product-card">
+            <img src={"https://www.biobizz.com/wp-content/uploads/2019/07/Bio%C2%B7pH-Product-Page-900x604.jpg"}
+                 className="product-img"   />
+                 <h5>Black Roze</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, excepturi.</p>
+                <button>Sepete Ekle</button>
+        </div>
     )
 }
 
