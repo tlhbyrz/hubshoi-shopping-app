89f2227184341ba5eb878f287bdd9f33cfb70efd
tlhbyrz <talhaboyraz13@gmail.com>
2020-08-20 16:49:10 +0300

    product listin responsive ayarlamaları yapıldı

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -9,7 +9,7 @@ function ProductCard() {
             <img src={"https://www.biobizz.com/wp-content/uploads/2019/07/Bio%C2%B7pH-Product-Page-900x604.jpg"}
                  className="product-img"   />
                  <h5>Black Roze</h5>
-                <p>Lorem ipsum dolor sit amet, consectetur.dolor sit amet, consectetur.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur.</p>
                 <p className="add-to-card-button">Sepete Ekle</p>
         </div>
     )
