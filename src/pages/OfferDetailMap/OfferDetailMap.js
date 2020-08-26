695e482cc86e929f77ad900992cdb2436cdf7ce1
tlhbyrz <talhaboyraz13@gmail.com>
2020-08-26 08:58:10 +0300

    tasarım için google map iframe eklendi

--- src/pages/OfferDetailMap/OfferDetailMap.js
+++ src/pages/OfferDetailMap/OfferDetailMap.js
@@ -9,7 +9,12 @@ function OfferDetailMap() {
             <MapPageHeader/>
             <div className="offer-detail-map-section">
                 <OfferLeftWithoutImg/>
-                <div className="offer-detail-map-location-section"></div>
+                <div className="offer-detail-map-location-section">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3009.4284814835873!2d29.088418315662725!3d41.03775802555852!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDHCsDAyJzE1LjkiTiAyOcKwMDUnMjYuMiJF!5e0!3m2!1str!2str!4v1598421112334!5m2!1str!2str"
+                        width="600" height="450" frameBorder="0" allowFullScreen=""
+                        aria-hidden="false" tabindex="0"></iframe>
+                </div>
             </div>
         </Fragment>
     )
